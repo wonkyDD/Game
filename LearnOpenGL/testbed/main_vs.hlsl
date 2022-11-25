d69bcaf0ce75b821dfb87eb8b8fc88d4e5e42753
wonkydd <dev.csh.01@gmail.com>
2022-11-26 02:21:30 +0900

    :truck: fix: change extension from `glsl` to `hlsl`

--- LearnOpenGL/testbed/main_vs.hlsl
+++ LearnOpenGL/testbed/main_vs.hlsl
@@ -15,7 +15,6 @@ uniform mat4 projection;
 void main()
 {
     Normal = mat3(transpose(inverse(model))) * aNormal;
-    // FragPos = vec3(model * vec4(aPos + abs(sin(time * 0.5 + TexCoord.y)) * abs(cos(time * 0.5 + TexCoord.x)) * Normal, 1.0));
     FragPos = vec3(model * vec4(aPos, 1.0));
     gl_Position = projection * view * vec4(FragPos, 1.0);
     TexCoord = aTexCoord;
