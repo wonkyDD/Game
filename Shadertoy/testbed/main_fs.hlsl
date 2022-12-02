6307728d606690a0f00a986ae0c09c8cdcbce90a
wonkydd <dev.csh.01@gmail.com>
2022-12-02 12:17:59 +0900

    :construction: WIP: `gl_Fragcoord` gradation shader

--- Shadertoy/testbed/main_fs.hlsl
+++ Shadertoy/testbed/main_fs.hlsl
@@ -11,5 +11,8 @@ void main()
 {
     // vec3 col = 0.5 + 0.5 * cos(time + ourPos.xyx);
     vec3 col = 0.5 + 0.5 * cos(time + ourPos.xyx + vec3(0, 2, 4));
+
+    // @TODO 예상과 다르게 이상한 출력
+    //vec3 col = 0.5 + 0.5 * cos(time + gl_FragCoord.xyx + vec3(0, 2, 4));
     FragColor = vec4(col, 1.0);
 }
