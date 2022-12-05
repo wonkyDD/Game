1b45938d84ec56297692046b10069275d0f8632c
wonkydd <dev.csh.01@gmail.com>
2022-12-05 15:04:04 +0900

    :heavy_plus_sign: add `ImGui`

--- Box2D/testbed/main.cpp
+++ Box2D/testbed/main.cpp
@@ -5,13 +5,40 @@ int main()
 	// @TODO Enum으로 에러분류
 	if (init() == -1) return -1;
 
+	// @TODO glslVersion과 nullptr
+	//createUI(g_mainWindow, nullptr);
+
+	// @TODO ImGui
+	//bool show_demo_window = true;
+	//bool show_another_window = false;
+	//ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	while (!glfwWindowShouldClose(g_mainWindow))
 	{
 		processInput(g_mainWindow);
+		glfwGetWindowSize(g_mainWindow, &g_camera.m_width, &g_camera.m_height);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// @TODO ImGui
+		//ImGui_ImplOpenGL3_NewFrame();
+		//ImGui_ImplGlfw_NewFrame();
+		//ImGui::NewFrame();
+		//ImGui::Render();
+		//ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
 		glfwSwapBuffers(g_mainWindow);
 		glfwPollEvents();
 	}
+
+	// @TODO ImGui
+	//ImGui_ImplOpenGL3_Shutdown();
+	//ImGui_ImplGlfw_Shutdown();
+	//ImGui::DestroyContext();
+
+	glfwDestroyWindow(g_mainWindow);
+	glfwTerminate();
+
 	return 0;
 }
---
+++ Box2D/testbed/types.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed char	int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+#endif // TYPES_H
