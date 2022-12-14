e6cebec84931ad250add6271ab39ba7d997a7293
wonkydd <dev.csh.01@gmail.com>
2022-12-15 04:22:59 +0900

    :sparkles: Init `LearnMAUI`

---
+++ LearnMAUI/README.md
@@ -0,0 +1,4 @@
+# LearnMAUI
+
+- UWP 정식배포 없이 hack으로 exe파일 실행법
+https://stackoverflow.com/questions/73718305/easiest-way-to-launch-a-maui-app-exe-on-windows
---
+++ LearnMAUI/console/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+
+string path = "D:\\dev\\Game\\LearnMAUI\\console\\test.txt";
+
+async Task ExampleAsync(string path)
+{
+    string[] lines = System.IO.File.ReadAllLines(path);
+    using StreamWriter file = new(path, append: true);
+
+    // await File.WriteAllLinesAsync(path, lines);
+    foreach (string line in lines)
+    {
+        await file.WriteLineAsync(line);
+    }
+}
+
+string[] lines = System.IO.File.ReadAllLines(path);
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
+
+await ExampleAsync(path);
+
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
