c44081bfa6bb1eb5a8a4676180f96c6f6b58e1ab
wonkydd <dev.csh.01@gmail.com>
2022-12-22 04:21:42 +0900

    :art::fire: update projects structure

--- LearnBackend/webapi/Program.cs
+++ LearnBackend/webapi/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,9 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
