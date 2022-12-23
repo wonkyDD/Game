c9544b6c328996133b7a357595ce4a16337e0585
wonkydd <dev.csh.01@gmail.com>
2022-12-23 19:03:26 +0900

    :bulb: update

--- LearnOpenGL/Pixel/main.cpp
+++ LearnOpenGL/Pixel/main.cpp
@@ -2,6 +2,16 @@
 
 int main()
 {
-	// @TODO beizer curve
-	//https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+	/*
+	* @TODO
+	* - https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+	* - https://stackoverflow.com/questions/8847899/how-to-draw-text-using-only-opengl-methods
+	* - https://www.youtube.com/watch?v=jvPPXbo87ds
+	* - https://stackoverflow.com/questions/5262951/what-is-state-of-the-art-for-text-rendering-in-opengl-as-of-version-4-1
+	* - https://www.youtube.com/watch?v=1b5hIMqz_wM
+	* - https://steamcdn-a.akamaihd.net/apps/valve/2007/SIGGRAPH2007_AlphaTestedMagnification.pdf
+	* - https://learnopengl.com/In-Practice/Text-Rendering
+	* - https://github.com/rougier/freetype-gl.git
+	* 
+	*/
 }
